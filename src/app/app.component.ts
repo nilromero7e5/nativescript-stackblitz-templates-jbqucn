547b55e7043a9184a1fa89f7f532ca364e197559
nilromero7e5 <94827970+nilromero7e5@users.noreply.github.com>
2023-03-14 17:29:38 +0100

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,13 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core';
+import Theme from '@nativescript/theme';
 
 @Component({
   selector: 'ns-app',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    Theme.setMode(Theme.Dark);
+  }
+}
